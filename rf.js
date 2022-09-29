1a47c8d88b1a9175f8f87213effc2e0fa068a03d
Gus3103 <114529643+Gus3103@users.noreply.github.com>
2022-09-29 16:47:41 -0300

    Add files via upload

--- rf.js
+++ rf.js
@@ -1,5 +1,6 @@
 
 let canvas1 = document.getElementById('canvas');
+const Bandas= ["R","L","M","O","P","N","B","T","F","U"]
 var bt = document.getElementById('b1');
 var col=123;
 console.log(bt)
@@ -18,7 +19,7 @@ ctx1=canvas1.getContext("2d")
 
 
 
-ctx1.fillStyle= "#141414";
+ctx1.fillStyle= "#FFFF00";
 ctx1.strokeStyle='#123'
 ctx1.lineWidth=1
 
@@ -27,8 +28,35 @@ ctx1.lineWidth=1
 function dibujar1(){
  ctx1.fillRect(10,10,50,30)
  ctx1.strokeRect(10,40,50,30)
- 
+ console.log(content.length)
 
 }
 
-dibujar1()
+
+
+function dibujar_bandas(){
+
+
+for(var i=0; i<10;i++){
+				ctx1.fillStyle= "#123";
+				ctx1.fillText(Bandas[i],20,10+10 *(1+i));
+				
+}	
+	
+for(var j=0; j<3;j++){
+	for(var k=0; k<4;k++){
+		for(var i=0; i<10;i++){
+				
+				ctx1.fillStyle= "#FFFF00";
+				ctx1.fillRect(30+j*100+k*20,(10 *(1+i))+2,20,8);
+				
+				ctx1.strokeStyle='#123';
+				ctx1.strokeRect(30+j*100+k*20,(10 *(1+i))+2,20,8);
+				
+		}
+	}
+}
+
+}
+
+dibujar_bandas()
