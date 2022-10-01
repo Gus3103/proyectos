de108944ee27bbca33b7f111c3a41383d71db45a
Gus3103 <114529643+Gus3103@users.noreply.github.com>
2022-10-01 18:38:11 -0300

    Add files via upload

--- lee.js
+++ lee.js
@@ -1,8 +1,10 @@
 const ArchivoExcel = document.getElementById("excel-ìnput")
 var content
-console.log(ArchivoExcel)
+// console.log(ArchivoExcel)
 const Bandas= ["R","L","M","O","P","N","B","T","F","U","V","W","G","M"]
 let Band= new Array(14);
+let PlacaRRU =new Array();
+let Presionados=new Array()
 for (var i=0 ;i<14 ; i++){
 
 	Band[i] =new Array(4)
@@ -14,32 +16,37 @@ for (var i=0 ;i<14 ; i++){
 	}
 }
 
-function Evaluar(celda,estado){
+Presionados.fill(0)
+function Evaluar(celda,estado,RRU){
 	let Car = celda.length
 	let Sector = parseInt(celda[Car-1])-1
 	let Port = parseInt(celda[Car-2])-1
 	let BandaRf = celda[Car-3]
 	let Ind =Bandas.indexOf(BandaRf)
- 	console.log(Ind,Port,Sector)
+ 	// console.log(Ind,Port,Sector)
  	if (estado =="OK"){
  		Band[Ind][Port][Sector]=1;
  	}
  	else{
  		Band[Ind][Port][Sector]=2;
  	}
-
- 	console.log(Band[Ind][Port][Sector]);
+ 	if (PlacaRRU.indexOf(RRU)<0){
+ 		PlacaRRU.push(RRU)
+ 		Presionados.push(0)
+ 	}
+ 	// console.log(PlacaRRU);
 }
 
 ArchivoExcel.addEventListener("change",async function leer(){
 	content = await readXlsxFile(ArchivoExcel.files[0]);
-	console.log(content);
-	console.log(content[0][1]);
-	console.log(content.length);
+	// console.log(content);
+	// console.log(content[0][1]);
+	// console.log(content.length);
 	for(var i=1; i< content.length;i++){
 		// console.log(content[i][0]);
-		Evaluar(content[i][0],content[i][1]);
+		Evaluar(content[i][0],content[i][1],content[i][3]);
 	}
 	dibujar_bandas()
+	Etiquetar_botones()
 })
 
--- rf.js
+++ rf.js
@@ -1,24 +1,50 @@
 
 let canvas1 = document.getElementById('canvas');
-
-// Band.fill(3)
-console.log(Band)
-var bt = document.getElementById('b1');
-var col=123;
-console.log(bt)
-var contador=0;
-function cambio(){
-	console.log("hola " + contador)
-	contador++;
-	ctx1.fillStyle= "#"+col;
-	col=col+10
-	console.log(bt.textContent)
-	bt.textContent="RRU : 60"
-	bt.style.backgroundColor="#ffff00"
-}
-bt.addEventListener('click',cambio,true)
-
 ctx1=canvas1.getContext("2d")
+// Band.fill(3)
+// console.log(Band)
+var Botones =new Array();
+
+var x=3
+var Etiqueta="";
+Etiqueta="b"+x
+
+// // Botones[3] = document.getElementById("b3");
+// var col=123;
+// // console.log(Botones[3])
+// var contador=0;
+// function cambio(){
+// 	console.log("hola cambio " + contador)
+// 	contador++;
+// 	ctx1.fillStyle= "#"+col;
+// 	col=col+10
+	
+	
+// 	// this.style.backgroundColor="#ffff00"
+// 	console.log(this.className)
+// 	for(var i =1 ; i <= PlacaRRU.length;i++){
+// 		if ("RRU "+i==this.className) Presionados[i-1]=1;
+// 		else Presionados[i-1]=0;
+// 	}
+// 	console.log(Presionados)
+// 	Etiquetar_botones()
+// }
+
+
+
+
+// for(var i =0 ; i<15 ; i++){
+// 	Etiqueta="b"+(i+1)
+// 	var func="cambio"+(i+1)
+// 	Botones[i]=document.getElementById(Etiqueta);
+// 	Botones[i].addEventListener('click',cambio,true)
+// 	console.log(Botones[i])
+// 	// console.log(Botones[0].textContent)
+// }
+
+
+
+
 
 
 
@@ -28,21 +54,21 @@ ctx1.lineWidth=1
 
 
 
-function dibujar1(){
- ctx1.fillRect(10,10,50,30)
- ctx1.strokeRect(10,40,50,30)
- console.log(content.length)
+// function dibujar1(){
+//  ctx1.fillRect(10,10,50,30)
+//  ctx1.strokeRect(10,40,50,30)
+//  // console.log(content.length)
 
-}
+// }
 
 
 
 function dibujar_bandas(){
 
 
-for(var i=0; i<10;i++){
+for(var i=0; i<14;i++){
 				ctx1.fillStyle= "#123";
-				ctx1.fillText(Bandas[i],20,10+10 *(1+i));
+				ctx1.fillText(Bandas[i],20,20+20 *(1+i));
 				
 }	
 	
@@ -51,19 +77,77 @@ for(var j=0; j<3;j++){
 		for(var i=0; i<14;i++){
 				ctx1.fillStyle= "#BDBDBD"
 
-				console.log(Band)
+				// console.log(Band)
 				if (Band[i][k][j]==0) {ctx1.fillStyle= "#BDBDBD"};
 				if (Band[i][k][j]==1) {ctx1.fillStyle= "#31B404"};
 				if (Band[i][k][j]==2){ ctx1.fillStyle= "#FF0000"};
-				ctx1.fillRect(30+j*200+k*35,(10 *(1+i))+2,35,8);
+				ctx1.fillRect(30+j*200+k*35,(20 *(1+i))+2,35,18);
 				
 				ctx1.strokeStyle='#123';
-				ctx1.strokeRect(30+j*200+k*35,(10 *(1+i))+2,35,10);
+				ctx1.strokeRect(30+j*200+k*35,(20 *(1+i))+2,35,20);
+
 				
 		}
 	}
 }
 
+
+function Dibujar_RRU(PosX,PosY){
+	ctx1.fillStyle= "#00ff00"
+	ctx1.strokeStyle='#123'
+	ctx1.fillRect(PosX,PosY,140,40);
+	ctx1.strokeRect(PosX,PosY,140,40);
+
+	for(var i=0;i<4;i++){
+		ctx1.fillRect(PosX+i*35,PosY-10,35,10);
+		ctx1.strokeRect(PosX+i*35,PosY-10,35,10)
+	}
+	ctx1.fillRect(PosX+20,PosY+40,40,10);
+	ctx1.fillRect(PosX+80,PosY+40,40,10);
+	ctx1.strokeRect(PosX+20,PosY+40,40,10);
+	ctx1.strokeRect(PosX+80,PosY+40,40,10);
+	ctx1.fillStyle= "#123"
+	ctx1.fillText("RRU :80",PosX+10,PosY+10);
 }
 
+
+for (var i=0 ;i<3;i++){
+	for (var j=0 ;j<5;j++){
+		Dibujar_RRU(30+i*200,400+j*70)
+	}
+}
+Dibujar_RRU(30,400)
+
+function Mover(event){
+	for (var i=0 ;i<3;i++){
+		for (var j=0 ;j<5;j++){
+			if(event.pageX> 30+i*200){
+				if(event.pageX< 30+i*200+140){
+					if(event.pageY> 400+j*70){
+						if(event.pageY< 400+j*70+40){
+							console.log("RRU " +(i+j*3+1))
+						}
+					}
+				}
+			}
+			Dibujar_RRU(30+i*200,400+j*70)
+		}
+	}
+	console.log(event.pageX)
+}
+
+document.addEventListener("mousemove", Mover)
+
+}
+
+// function Etiquetar_botones(){
+
+// 	for (var i=0; i<PlacaRRU.length;i++){
+// 		// console.log(Botones[i+1].textContent)
+// 		Botones[i].textContent="RRU : " +PlacaRRU[i];
+// 		if (Presionados[i]==1) Botones[i].style.backgroundColor="#F7FE2E"
+// 		else Botones[i].style.backgroundColor="#F2F2F2"
+
+// 	}
+// }
 dibujar_bandas()
